85db4f3ff098890d03ff584727df41ac402f5301
d0422 <rlfehd2013@naver.com>
2023-03-13 20:55:14 +0900

    [FEAT] router

--- expressStudy/src/app.js
+++ expressStudy/src/app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-app.set('port', 3000);
-app.get(
-  '/',
-  (req, res, next) => {
-    console.log('GET요청이네용');
-    res.sendFile(path.join(__dirname, '/index.html'));
-    next();
-  },
-  (req, res) => {
-    throw new Error('에러는 에러처리 미들웨어로 갑니다.');
-  }
-);
+app.set('port', process.env.PORT || 3000);
+
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use(morgan('dev'));
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트로 서버 열림');
+});
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
 });
 app.use((err, req, res, next) => {
   console.error(err);
---
+++ expressStudy/src/routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.send('Hello, User');
+});
+router.get('/:id', (req, res) => {
+  console.log(req.params, req.query);
+  res.send(req.params.id);
+});
+module.exports = router;
