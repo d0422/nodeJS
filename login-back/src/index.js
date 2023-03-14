5a8b6fa9c74842c615b2454c9341e0a4bbe6591d
d0422 <rlfehd2013@naver.com>
2023-03-15 01:59:06 +0900

    [FEAT] 로그인을 위한 백엔드 구성

---
+++ login-back/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.set('port', 3000);
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트로 서버 ON');
+});
+const defaultRouter = require('./routes/index');
+app.use('/', defaultRouter);
---
+++ login-back/src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('하이');
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+});
+module.exports = router;
