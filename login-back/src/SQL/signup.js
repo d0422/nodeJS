c6d0d1a01d99c3593cc28c16e93fe403d2cfed7b
d0422 <rlfehd2013@naver.com>
2023-03-15 15:25:24 +0900

    [FEAT] signup시 salt 뿌려서 hash한 것을 db에 저장하도록 변경

--- login-back/src/SQL/signup.js
+++ login-back/src/SQL/signup.js
@@ -1,7 +1,13 @@
 const connectMySQL = require('./connect');
+const crypto = require('crypto');
 const signup = async (id, pw, name, age) => {
+  const salt = crypto.randomBytes(128).toString('base64'); //salt 생성
+  const hasedPassword = crypto
+    .createHash('sha512')
+    .update(pw + salt)
+    .digest('hex');
   const connect = await connectMySQL();
-  const signupSQL = `INSERT INTO login_table(id,pw,name,age) values('${id}', '${pw}', '${name}', '${age}')`;
+  const signupSQL = `INSERT INTO login_table(id,pw,name,age,salt) values('${id}', '${hasedPassword}', '${name}', '${age}',${salt})`;
   connect.execute(signupSQL);
 };
 module.exports = signup;
