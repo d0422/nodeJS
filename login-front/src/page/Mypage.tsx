b458549f4bf3aee8de76c75b38cd902fcd47c49f
d0422 <rlfehd2013@naver.com>
2023-03-23 16:24:41 +0900

    [FEAT] profile 데이터 렌더링

--- login-front/src/page/Mypage.tsx
+++ login-front/src/page/Mypage.tsx
@@ -1,18 +1,33 @@
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import access from '../atom/token';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import getProfile from '../api/profile';
 import styled from 'styled-components';
 const Mypage = () => {
   const access_token = useRecoilValue(access);
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<any>();
   const navigate = useNavigate();
-  getProfile(access_token);
+  useEffect(() => {
+    getProfile(access_token).then((res) => {
+      setData(res);
+      setLoading(false);
+    });
+  }, []);
+
   if (!access_token) {
     navigate('/');
   }
-  return <Wrapper>{access_token}</Wrapper>;
+  if (loading) return <Wrapper>로딩중</Wrapper>;
+  return (
+    <Wrapper>
+      <ProfileWrapper>
+        <Name>{data?.name} 님</Name>
+        <Name>{data?.age} 세</Name>
+      </ProfileWrapper>
+    </Wrapper>
+  );
 };
 
 export default Mypage;
@@ -24,4 +39,18 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   background-color: #eeeeee;
+`;
+const Name = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+`;
+const ProfileWrapper = styled.div`
+  width: 400px;
+  height: 700px;
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 40px;
+  font-family: 'OTWelcomeRA';
 `;
